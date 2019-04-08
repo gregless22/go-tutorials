c1b6636ef4c2c4c8531f03acb87c1428b72097aa
Greg Connolly <Gregoryconnolly@gmail.com>
2019-04-08 20:26:00 +1000

    end

---
+++ go-cli/cmd/my-cli/cli.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+var (
+	pl = fmt.Println
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Website Lookup CLI"
+	app.Usage = "Lets you query IPs, CNAMES, MX records"
+
+	// flags.
+	myFlags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "jamesconnolly.com.au",
+		},
+	}
+
+	//create the commands
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "ns",
+			Usage: "Looks Up the NameServers for a Particular Host",
+			Flags: myFlags,
+			// the action, or code that will be executed when
+			// we execute our `ns` command
+			Action: func(c *cli.Context) error {
+				ns, err := net.LookupNS(c.String("url"))
+				if err != nil {
+					return err
+				}
+				for _, item := range ns {
+					pl(item.Host)
+				}
+				return nil
+			},
+		},
+		{
+			Name:  "ip",
+			Usage: "Looks up the IP addresses for a particular host",
+			Flags: myFlags,
+			Action: func(c *cli.Context) error {
+				ip, err := net.LookupIP(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+				}
+				for i := 0; i < len(ip); i++ {
+					fmt.Println(ip[i])
+				}
+				return nil
+			},
+		},
+		{
+			Name:  "cname",
+			Usage: "Looks up the CNAME for a particular host",
+			Flags: myFlags,
+			Action: func(c *cli.Context) error {
+				cname, err := net.LookupCNAME(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+				}
+				fmt.Println(cname)
+				return nil
+			},
+		},
+		{
+			Name:  "mx",
+			Usage: "Looks up the MX records for a particular host",
+			Flags: myFlags,
+			Action: func(c *cli.Context) error {
+				mx, err := net.LookupMX(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+				}
+				for i := 0; i < len(mx); i++ {
+					fmt.Println(mx[i].Host, mx[i].Pref)
+				}
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
