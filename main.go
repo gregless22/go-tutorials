245193433418809f30b942c2b8f32ac93fd828b4
Greg Connolly <Gregoryconnolly@gmail.com>
2019-04-09 19:24:26 +1000

    1st example end

--- main.go
+++ main.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-var pl = fmt.Println
+var (
+	pf = fmt.Printf
+	pl = fmt.Println
+)
 
 func main() {
+	coolFunc(myFunc)
+}
 
+func myFunc() {
+	pl("Hello World")
+	time.Sleep(1 * time.Second)
+}
+
+func coolFunc(a func()) {
+	pf("Starting function execution: %s\n", time.Now())
+	a()
+	pf("End of function execution: %s\n", time.Now())
 }
