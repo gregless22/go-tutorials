ba1df32d14efed049454f57bdbfda9f13bf93676
Greg Connolly <Gregoryconnolly@gmail.com>
2019-04-09 20:25:09 +1000

    end

--- main.go
+++ main.go
@@ -6,11 +6,41 @@ import (
 )
 
 var (
-	mutex sync.Mutex
-	pf    = fmt.Printf
-	pl    = fmt.Println
+	mutex   sync.Mutex
+	pf      = fmt.Printf
+	pl      = fmt.Println
+	balance int
 )
 
 func main() {
+	pl("Mutex Example")
 
+	done := make(chan bool)
+	go withdraw(700, done)
+	go deposit(500, done)
+
+	<-done
+	<-done
+
+	pl("final balance", balance)
+}
+
+func init() {
+	balance = 1000
+}
+
+func deposit(value int, done chan bool) {
+	mutex.Lock()
+	balance += value
+	mutex.Unlock()
+	done <- true
+	pl("deposit", balance)
+}
+
+func withdraw(value int, done chan bool) {
+	mutex.Lock()
+	balance -= value
+	mutex.Unlock()
+	done <- true
+	pl("witdraw", balance)
 }
