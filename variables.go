2ed272cf461644e38907bb59a820f07a0fb3dafe
Greg Connolly <Gregoryconnolly@gmail.com>
2019-09-02 20:04:59 +1000

    string manipulaton

---
+++ variables.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	str1 := "An implicitly typed string"
+	fmt.Printf("str1: %v: %T\n", str1, str1)
+
+	str2 := "An explicitly typed string"
+	fmt.Printf("str1: %v: %T\n", str2, str2)
+
+	fmt.Println(strings.ToUpper(str1))
+	fmt.Println(strings.Title(str1))
+}
